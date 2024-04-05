33559d5b6f96e7fb3bb516db52ab9699c28cab63
manishahaha09 <manishavenkatesha03@gmail.com>
2024-04-05 11:31:42 +0530

    Initial commit

---
+++ camera.py
@@ -0,0 +1,12 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+
+cap= cv2.VideoCapture(0)
+detector= HandDetector(maxHands=1)
+
+
+while True:
+    success, img = cap.read()
+    hands, img =detector.findHands(img)
+    cv2.imshow("Image",img)
+    cv2.waitKey(1)
